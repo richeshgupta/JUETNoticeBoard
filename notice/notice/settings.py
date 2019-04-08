f6c91f733cd62bcc459da324fcc0bcfe03c53fb8
Satyam <satyamupadhyay260@gmail.com>
2019-04-09 01:41:01 +0530

    Added Email notification settings

--- notice/notice/settings.py
+++ notice/notice/settings.py
@@ -123,3 +123,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = 'notice'
+
+EMAIL_HOST          = 'smtp.gmail.com'
+EMAIL_PORT          = 587
+EMAIL_HOST_USER     = 'pantomath.notice@gmail.com'
+EMAIL_HOST_PASSWORD = 'pantomath123'
+EMAIL_USE_TLS       = True
+EMAIL_USE_SSL       = False
