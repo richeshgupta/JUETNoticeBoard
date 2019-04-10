77784f5a5dba9774ae854f0c02821e435b2745d2
Richesh Gupta <rgrichesh45@gmail.com>
2019-04-10 08:30:30 +0530

    Email is now template based yet problem of posting and emailing simultaneous is still an issue

--- notice/send/views.py
+++ notice/send/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
-from django.core.mail import send_mail
-
+from django.core.mail import send_mail,EmailMessage
+from django.template.loader import get_template
 # Create your views here.
+
 def index_mail(request):
-	send_mail('Notice Board',
-			'New Post has been Added',
-			'pantomath.notice@gmail.com',
-			['satyamupadhyay260@gmail.com','rgrichesh45@gmail.com'],
-			fail_silently = False)
-	return render(request, 'send/index.html',{})
+	subject = "New Post is up - JUET Notice Board"
+	to_mail = ['rgrichesh45@gmail.com','satyamupadhyay260@gmail.com']
+	from_acc = 'pantomath.notice@gmail.com'
+	message = get_template('send/mail_temp.html').render()
+	msg = EmailMessage(subject,message,to=to_mail,from_email=from_acc)
+	msg.content_subtype='html'
+	msg.send()
+	return render(request,'send/index.html',{})
